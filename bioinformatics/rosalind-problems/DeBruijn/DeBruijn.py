a4273442ba445a3752a65b567f5e724fe668ffe4
larkenx <larkenx@gmail.com>
2016-10-17 13:46:45 -0400

    C files

--- bioinformatics/rosalind-problems/DeBruijn/DeBruijn.py
+++ bioinformatics/rosalind-problems/DeBruijn/DeBruijn.py
@@ -1,36 +1,29 @@
 import filecmp
 
-def printHelper(arr, i):
+def printHelper(arr):
     result = ""
-    for x in arr[i]:
-        result = result + x + ","
+    for x in arr:
+        result += x
+        result += ","
     return result[:-1] # strip the last comma and whitespace
 
 def DeBruijn(fileName):
     f = open(fileName, "r")
+    result = dict()
     patterns = [x.strip('\n') for x in f.readlines()]
     f.close()
     patterns.sort()
-    prefixes = []
     for pattern in patterns:
         current = pattern[:-1]
-        if (not current in prefixes):
-            prefixes.append(current)
-    # Now we have all the possible prefixes
-    result = list([] for foo in xrange(len(prefixes)))
-    for pattern in patterns:
-        current = pattern[:-1]
-        if (current in prefixes):
-            result[prefixes.index(current)].append(pattern[1:])
+        if (not current in result):
+            result[current] = list()
+        result[current].append(pattern[1:])
 
     # Print our result
     f = open("output.txt", 'w')
     f.truncate()
-    for x in range(len(prefixes)):
-        f.write(prefixes[x])
-        f.write(" -> ")
-        f.write(printHelper(result, x))
-        f.write('\n')
+    for k,v in sorted(result.items()):
+        toWrite = k + " -> " + printHelper(v) + '\n'
+        f.write(toWrite)
 
-DeBruijn("sample.txt")
-# print filecmp.cmp("output.txt", "proper_output.txt")
+DeBruijn("data.txt")
