2ce141e909f8a2e2225e7efcdf9974ae45146e08
larkenx <larkenx@gmail.com>
2016-10-29 15:25:34 -0400

    Automated commit

--- bioinformatics/rosalind-problems/LCS/LCS.py
+++ bioinformatics/rosalind-problems/LCS/LCS.py
@@ -1,7 +1,6 @@
 # 0 == down, 1 == right, 2 == diag
 
 def getMax(s, i, j, match):
-    print "(" + str(i) +"," + str(j) + ")"
     a = s[i - 1][j]
     b = s[i][j - 1]
     c = s[i - 1][j - 1] + match
@@ -14,10 +13,10 @@ def getMax(s, i, j, match):
         return (-1, -1)
 
 def LCSBackTrack(s1, s2):
-    s = [[0 for y in range(len(s1) + 2)] for x in range((len(s2) + 2))]
-    backTrack = [[0 for y in range(len(s1) + 2)] for x in range((len(s2) + 2))]
-    v = len(s1)
-    w = len(s2)
+    s = [[0 for y in range(len(s2) + 1)] for x in range((len(s1) + 1))]
+    backTrack = [[0 for y in range(len(s1) + 1)] for x in range((len(s2) + 1))]
+    v = len(s2)
+    w = len(s1)
     for i in range(0, v+1):
         s[0][i] = 0
 
@@ -26,17 +25,15 @@ def LCSBackTrack(s1, s2):
 
     for i in range(1, v+1):
         for j in range(1, w+1):
-            if (s1[i-1] == s2[j-1]):
+            if (s2[i-1] == s1[j-1]):
                 match = 1
             else:
                 match = 0
 
             tup = getMax(s, i, j, match)
-            adjustment = 0
+            s[i][j] = s[i + tup[0]][j + tup[1]]
             if (tup == (-1, -1)):
-                adjustment = 1
-
-            s[i][j] = s[i + tup[0]][j + tup[1]] + adjustment
+                s[i][j] += match
 
             if (tup == (-1, -1) and match == 1):
                 backTrack[i][j] = 2
@@ -45,6 +42,7 @@ def LCSBackTrack(s1, s2):
             elif (tup == (0, -1)):
                 backTrack[i][j] = 1
 
+    print "new matrix"
     for x in s:
         print x
 
@@ -67,13 +65,13 @@ def OutputLCS(backTrack, v, i, j):
         OutputLCS(backTrack, v, i, j - 1)
     else:
         OutputLCS(backTrack, v, i - 1, j - 1)
-        buf = v[i] + buf
+        buf = v[i-1] + buf
 
-s1 = "AACCTTGG"
-s2 = "ACACTGTGA"
+s1 = "AA"
+s2 = "AA"
 print len(s1)
 print len(s2)
-OutputLCS(LCSBackTrack(s2,s1), s1, len(s1) , len(s2))
+OutputLCS(LCSBackTrack(s1,s2), s1, len(s1) , len(s2))
 f = open("output.txt", "w")
 print buf
 f.write(buf)
