f1b01ced3bb4a7adcc231117c90e15ee51c58a3e
Steven Myers <stelmyer@umail.iu.edu>
2016-11-08 17:26:25 -0500

    updating win prog

---
+++ windows-programming/Collections/Collections/frmBuffetOptions.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Collections
+{
+    partial class frmBuffetOptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtUserNae = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(356, 40);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(78, 47);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(356, 110);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(78, 47);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(17, 40);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(202, 20);
+            this.lblUserName.TabIndex = 2;
+            this.lblUserName.Text = "This is some awesome text.";
+            this.lblUserName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtUserNae
+            // 
+            this.txtUserNae.Location = new System.Drawing.Point(21, 72);
+            this.txtUserNae.Multiline = true;
+            this.txtUserNae.Name = "txtUserNae";
+            this.txtUserNae.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtUserNae.Size = new System.Drawing.Size(198, 118);
+            this.txtUserNae.TabIndex = 3;
+            this.txtUserNae.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+            // 
+            // frmBuffetOptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(456, 347);
+            this.Controls.Add(this.txtUserNae);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmBuffetOptions";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "A290 Buffet Options";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        public System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox txtUserNae;
+    }
+}
