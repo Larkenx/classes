e74e385ab7f743fdb33b0a94a3b641c84b792f96
Steven Myers <stelmyer@umail.iu.edu>
2016-11-13 16:23:54 -0500

    updated project proposal and pagerank project

---
+++ windows-programming/PageRankProject/PageRankProject/PageRank.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PageRankProject
+{
+    partial class PageRank
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_DisplayLinks = new System.Windows.Forms.TextBox();
+            this.txt_targetPage = new System.Windows.Forms.TextBox();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_DisplayLinks
+            // 
+            this.txt_DisplayLinks.Location = new System.Drawing.Point(13, 50);
+            this.txt_DisplayLinks.Multiline = true;
+            this.txt_DisplayLinks.Name = "txt_DisplayLinks";
+            this.txt_DisplayLinks.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_DisplayLinks.Size = new System.Drawing.Size(665, 330);
+            this.txt_DisplayLinks.TabIndex = 0;
+            // 
+            // txt_targetPage
+            // 
+            this.txt_targetPage.Location = new System.Drawing.Point(13, 12);
+            this.txt_targetPage.Name = "txt_targetPage";
+            this.txt_targetPage.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txt_targetPage.Size = new System.Drawing.Size(167, 20);
+            this.txt_targetPage.TabIndex = 1;
+            this.txt_targetPage.Text = "Type in a URL and click search...";
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(187, 12);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(84, 20);
+            this.btn_Search.TabIndex = 2;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // PageRank
+            // 
+            this.AcceptButton = this.btn_Search;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 392);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.txt_targetPage);
+            this.Controls.Add(this.txt_DisplayLinks);
+            this.Name = "PageRank";
+            this.Text = "Page Rank Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_DisplayLinks;
+        private System.Windows.Forms.TextBox txt_targetPage;
+        private System.Windows.Forms.Button btn_Search;
+    }
+}
+
---
+++ windows-programming/PageRankProject/PageRankProject/PageRank.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace PageRankProject
+{
+    public partial class PageRank : Form
+    {
+        public PageRank()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            // Our user has just conducted a new search, so we need to clear the previous text display
+            txt_DisplayLinks.Text = "";
+
+            // Create a new HTML Web object to get our HTML document
+            HtmlWeb WebGet = new HtmlWeb();
+
+            // Create new HTML Document with the HtmlAgilityPack library
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+
+            // Fix nested tags with doc options
+            doc.OptionFixNestedTags = true;
+
+            // Now load the URL given by the user in the text box
+            string url = txt_targetPage.Text;
+
+            // Let's prepare a list of strings to store our links from the HTML document we've loaded
+            List<string> links = new List<string>();
+
+            // Load a web page. We need to put this in a try/catch statement in case our user has put in some invalid input
+            try
+            {
+                doc = WebGet.Load(url);
+                // We need to iterate through every 'htmlnode' object in our document
+                // We create a collection of htmlnodes by using the .SelectNodes method on the documentNode,
+                // and passing this function an Xpath query.
+                if (doc.ParseErrors != null && doc.ParseErrors.Count() > 0)
+                {
+                    // If we have any issues parsing the html document, we can analyze them here
+                    foreach (HtmlAgilityPack.HtmlParseError error in doc.ParseErrors)
+                    {
+                        txt_DisplayLinks.AppendText(error.ToString() + '\n');
+                    }
+
+                }
+                else
+                {
+                    // Create a collection of the nodes
+                    HtmlNodeCollection query = doc.DocumentNode.SelectNodes("//a[@href]");
+
+                    foreach (HtmlNode link in query)
+                    {
+                        // Now we just need to grab the attribute from the htmlnode
+                        HtmlAttribute att = link.Attributes["href"];
+                        // and add it to our list of links
+                        links.Add(att.Value);
+                    }
+
+                    // We now have a list of all the links on a webpage.
+                    // Let's load them into our text box in the form.
+                    foreach (string link in links)
+                    {
+                        txt_DisplayLinks.AppendText(link + '\n');
+                    }
+
+                }
+            }
+            catch (System.UriFormatException evt)
+            {
+                txt_DisplayLinks.AppendText(evt.ToString() + '\n');
+            }
+
+        }
+    }
+}
