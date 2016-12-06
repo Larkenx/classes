3dbe71e9b93558fc32a7534b8552a4cb93bab72d
Steven Myers <stelmyer@umail.iu.edu>
2016-12-06 17:27:33 -0500

    got it working

--- windows-programming/PageRankProject/PageRankProject/PageRank.Designer.cs
+++ windows-programming/PageRankProject/PageRankProject/PageRank.Designer.cs
@@ -32,37 +32,41 @@
             this.txt_targetPage = new System.Windows.Forms.TextBox();
             this.btn_Search = new System.Windows.Forms.Button();
             this.cbo_RecursionDepth = new System.Windows.Forms.ComboBox();
-            this.picBox_ShowVisualization = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.picBox_ShowVisualization)).BeginInit();
+            this.TabsContainer = new System.Windows.Forms.TabControl();
+            this.LogsTab = new System.Windows.Forms.TabPage();
+            this.GraphTab = new System.Windows.Forms.TabPage();
+            this.TabsContainer.SuspendLayout();
+            this.LogsTab.SuspendLayout();
             this.SuspendLayout();
             // 
             // txt_DisplayLinks
             // 
+            this.txt_DisplayLinks.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txt_DisplayLinks.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_DisplayLinks.Location = new System.Drawing.Point(20, 175);
-            this.txt_DisplayLinks.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_DisplayLinks.Location = new System.Drawing.Point(3, 3);
             this.txt_DisplayLinks.Multiline = true;
             this.txt_DisplayLinks.Name = "txt_DisplayLinks";
             this.txt_DisplayLinks.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txt_DisplayLinks.Size = new System.Drawing.Size(754, 492);
+            this.txt_DisplayLinks.Size = new System.Drawing.Size(1031, 398);
             this.txt_DisplayLinks.TabIndex = 0;
             // 
             // txt_targetPage
             // 
-            this.txt_targetPage.Location = new System.Drawing.Point(173, 14);
-            this.txt_targetPage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_targetPage.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.txt_targetPage.Location = new System.Drawing.Point(423, 12);
             this.txt_targetPage.Name = "txt_targetPage";
             this.txt_targetPage.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
-            this.txt_targetPage.Size = new System.Drawing.Size(385, 26);
+            this.txt_targetPage.Size = new System.Drawing.Size(258, 20);
             this.txt_targetPage.TabIndex = 1;
             this.txt_targetPage.Text = "Type in a URL and click search...";
+            this.txt_targetPage.Click += new System.EventHandler(this.txt_targetPage_Click);
             // 
             // btn_Search
             // 
-            this.btn_Search.Location = new System.Drawing.Point(233, 96);
-            this.btn_Search.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Search.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btn_Search.Location = new System.Drawing.Point(490, 65);
             this.btn_Search.Name = "btn_Search";
-            this.btn_Search.Size = new System.Drawing.Size(246, 38);
+            this.btn_Search.Size = new System.Drawing.Size(143, 25);
             this.btn_Search.TabIndex = 2;
             this.btn_Search.Text = "Search";
             this.btn_Search.UseVisualStyleBackColor = true;
@@ -70,6 +74,7 @@
             // 
             // cbo_RecursionDepth
             // 
+            this.cbo_RecursionDepth.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cbo_RecursionDepth.FormattingEnabled = true;
             this.cbo_RecursionDepth.Items.AddRange(new object[] {
             "0",
@@ -78,38 +83,61 @@
             "3",
             "4",
             "5"});
-            this.cbo_RecursionDepth.Location = new System.Drawing.Point(171, 54);
-            this.cbo_RecursionDepth.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cbo_RecursionDepth.Location = new System.Drawing.Point(422, 38);
             this.cbo_RecursionDepth.Name = "cbo_RecursionDepth";
-            this.cbo_RecursionDepth.Size = new System.Drawing.Size(386, 28);
+            this.cbo_RecursionDepth.Size = new System.Drawing.Size(259, 21);
             this.cbo_RecursionDepth.TabIndex = 3;
             this.cbo_RecursionDepth.Text = "Select a Recursion Depth...";
             // 
-            // picBox_ShowVisualization
+            // TabsContainer
             // 
-            this.picBox_ShowVisualization.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.picBox_ShowVisualization.Location = new System.Drawing.Point(797, 14);
-            this.picBox_ShowVisualization.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.picBox_ShowVisualization.Name = "picBox_ShowVisualization";
-            this.picBox_ShowVisualization.Size = new System.Drawing.Size(554, 653);
-            this.picBox_ShowVisualization.TabIndex = 4;
-            this.picBox_ShowVisualization.TabStop = false;
+            this.TabsContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TabsContainer.Controls.Add(this.LogsTab);
+            this.TabsContainer.Controls.Add(this.GraphTab);
+            this.TabsContainer.Location = new System.Drawing.Point(12, 93);
+            this.TabsContainer.Name = "TabsContainer";
+            this.TabsContainer.SelectedIndex = 0;
+            this.TabsContainer.Size = new System.Drawing.Size(1045, 430);
+            this.TabsContainer.TabIndex = 4;
+            // 
+            // LogsTab
+            // 
+            this.LogsTab.Controls.Add(this.txt_DisplayLinks);
+            this.LogsTab.Location = new System.Drawing.Point(4, 22);
+            this.LogsTab.Name = "LogsTab";
+            this.LogsTab.Padding = new System.Windows.Forms.Padding(3);
+            this.LogsTab.Size = new System.Drawing.Size(1037, 404);
+            this.LogsTab.TabIndex = 0;
+            this.LogsTab.Text = "Logs";
+            this.LogsTab.UseVisualStyleBackColor = true;
+            // 
+            // GraphTab
+            // 
+            this.GraphTab.Location = new System.Drawing.Point(4, 22);
+            this.GraphTab.Name = "GraphTab";
+            this.GraphTab.Padding = new System.Windows.Forms.Padding(3);
+            this.GraphTab.Size = new System.Drawing.Size(1037, 404);
+            this.GraphTab.TabIndex = 1;
+            this.GraphTab.Text = "Graph View";
+            this.GraphTab.UseVisualStyleBackColor = true;
             // 
             // PageRank
             // 
             this.AcceptButton = this.btn_Search;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1364, 681);
-            this.Controls.Add(this.picBox_ShowVisualization);
+            this.ClientSize = new System.Drawing.Size(1069, 535);
+            this.Controls.Add(this.TabsContainer);
             this.Controls.Add(this.cbo_RecursionDepth);
             this.Controls.Add(this.btn_Search);
             this.Controls.Add(this.txt_targetPage);
-            this.Controls.Add(this.txt_DisplayLinks);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "PageRank";
             this.Text = "Page Rank Application";
-            ((System.ComponentModel.ISupportInitialize)(this.picBox_ShowVisualization)).EndInit();
+            this.TabsContainer.ResumeLayout(false);
+            this.LogsTab.ResumeLayout(false);
+            this.LogsTab.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,7 +149,9 @@
         private System.Windows.Forms.TextBox txt_targetPage;
         private System.Windows.Forms.Button btn_Search;
         private System.Windows.Forms.ComboBox cbo_RecursionDepth;
-        private System.Windows.Forms.PictureBox picBox_ShowVisualization;
+        private System.Windows.Forms.TabControl TabsContainer;
+        private System.Windows.Forms.TabPage LogsTab;
+        private System.Windows.Forms.TabPage GraphTab;
     }
 }
 
--- windows-programming/PageRankProject/PageRankProject/PageRank.cs
+++ windows-programming/PageRankProject/PageRankProject/PageRank.cs
@@ -16,7 +16,6 @@ namespace PageRankProject
     public partial class PageRank : Form
     {
 
-
         public PageRank()
         {
             InitializeComponent();
@@ -46,37 +45,18 @@ namespace PageRankProject
                 // We need to iterate through every 'htmlnode' object in our document
                 // We create a collection of htmlnodes by using the .SelectNodes method on the documentNode,
                 // and passing this function an Xpath query.
-                if (doc.ParseErrors != null && doc.ParseErrors.Count() > 0)
+                // Create a collection of the nodes
+                HtmlNodeCollection query = doc.DocumentNode.SelectNodes("//a[@href]");
+
+                foreach (HtmlNode link in query)
                 {
-                    // If we have any issues parsing the html document, we can analyze them here
-                    foreach (HtmlAgilityPack.HtmlParseError error in doc.ParseErrors)
-                    {
-                        result.Add(error.Reason.ToString() + '\n');
-                    }
-
-                }
-                else
-                {
-                    // Create a collection of the nodes
-                    HtmlNodeCollection query = doc.DocumentNode.SelectNodes("//a[@href]");
-
-                    foreach (HtmlNode link in query)
-                    {
-                        // Now we just need to grab the attribute from the htmlnode
-                        HtmlAttribute att = link.Attributes["href"];
-                        // and add it to our list of links
-                        result.Add(att.Value);
-                    }
-
-                    // We now have a list of all the links on a webpage.
-                    // Let's load them into our text box in the form.
-                    foreach (string link in result)
-                    {
-                        txt_DisplayLinks.AppendText(link + '\n');
-                    }
-
+                    // Now we just need to grab the attribute from the htmlnode
+                    HtmlAttribute att = link.Attributes["href"];
+                    // and add it to our list of links
+                    result.Add(att.Value);
                 }
             }
+
             catch (System.UriFormatException evt)
             {
                 result.Add(evt.ToString() + '\n');
@@ -90,6 +70,22 @@ namespace PageRankProject
             // Our user has just conducted a new search, so we need to clear the previous text display
             txt_DisplayLinks.Text = "";
 
+            // We also need to dispose of the last graph they drew, if they drew one
+            // We can only have a single control in our graph tab, so if there's one there
+            // We know it's the last graph
+            if (GraphTab.Controls.Count > 0)
+            {
+                // just to be certain, let's check that it's a graph viewer
+                if (GraphTab.Controls[0].GetType() == typeof(GViewer))
+                {
+                    // if it is, let's dispose it to save memory
+                    ((GViewer) GraphTab.Controls[0]).Dispose();
+                }
+
+            }
+            // finally, clear out all the controls after they've been disposed
+            GraphTab.Controls.Clear();
+
             // Using the link they provided, search for the results.
             List<String> results = retrieveLinks(txt_targetPage.Text);
 
@@ -99,27 +95,28 @@ namespace PageRankProject
             // edges from the root node (searched node).
 
             Graph graph = new Graph("graph");
+            graph.Directed = true;
+            graph.LayoutAlgorithmSettings = new Microsoft.Msagl.Layout.MDS.MdsLayoutSettings();
             graph.AddNode(txt_targetPage.Text);
 
             // Put the results in the text box for the user to see
             foreach (String result in results)
             {
-                txt_DisplayLinks.AppendText(result);
+                txt_DisplayLinks.AppendText(result + '\n');
                 graph.AddNode(result);
                 graph.AddEdge(txt_targetPage.Text, result);
             }
 
             GViewer viewer = new GViewer();
             viewer.Graph = graph;
-            //associate the viewer with the form 
-            SuspendLayout();
             viewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            Controls.Add(viewer);
-            ResumeLayout();
-            //show the form 
-            ShowDialog();
+            GraphTab.Controls.Add(viewer);
+        }
 
-
+        private void txt_targetPage_Click(object sender, EventArgs e)
+        {
+            // Clear the initial text
+            txt_targetPage.Text = "";
         }
     }
 }
