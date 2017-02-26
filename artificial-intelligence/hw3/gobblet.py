88b2bb04d3c5a0221d76131441434e1d35eb885a
larkenx <larkenx@gmail.com>
2017-02-26 13:40:55 -0500

    Automated commit

--- artificial-intelligence/hw3/gobblet.py
+++ artificial-intelligence/hw3/gobblet.py
@@ -21,6 +21,7 @@ class Tile:
         self.x = x
         self.y = y
         self.external = external
+        self.name = "Tile"
 
     def __repr__(self):
         return self
@@ -135,12 +136,13 @@ class Player:
             """
             if (not other_piece.owner == self.id) and other_piece.size < piece_to_move.size: # piece is not our own and is smaller
                 if inp[0] == "S": # piece came from stack
-                    valid_pieces = game.threes(other_piece.owner) # enemy piece is one of a three-in-a-row
+                    valid_pieces = [x.peek() for x in game.threes(other_piece.owner)] # enemy piece is one of a three-in-a-row
                     if other_piece in valid_pieces:
                         print "Moved " + str(loc1) + " to " + str(inp2)
                         print "You covered up an enemy piece!"
                         raw_input("[Press Enter to end turn]")
                         loc2.push(loc1.pop())
+                        game.history.append((loc1, loc2))
                     else:
                         print "You can't move a piece from your external stack to cover"
                         print "an enemy piece, unless that piece is a member of a three-in-a-row!"
@@ -152,12 +154,16 @@ class Player:
                     print "You covered up an enemy piece!"
                     raw_input("[Press Enter to end turn]")
                     loc2.push(loc1.pop())
+                    game.history.append((loc1, loc2))
 
             elif other_piece.size < piece_to_move.size: # covering one of our own pieces that is smaller
                 print "Moved " + str(loc1) + " to " + str(inp2) + "."
                 print "You covered up one of your own pieces!"
                 raw_input("[Press Enter to end turn]")
                 loc2.push(loc1.pop())
+                game.history.append((loc1, loc2))
+
+
             else: # invalid move
                 print "You cannot cover up a larger piece with a smaller or equally sized piece!"
                 print "Please try a different move."
@@ -167,6 +173,7 @@ class Player:
             print "Moved " + str(loc1) + " to " + str(inp2) + "."
             raw_input("[Press Enter to end turn]")
             loc2.push(loc1.pop())
+            game.history.append((loc1, loc2))
 
         loc2.peek().external_piece = False # if we move a piece, it must not be in the external stack anymore
         return self
@@ -177,8 +184,23 @@ class Robot(Player):
 
     # Overriding play method
     def play(self, game):
-        # return self.alpha_beta_search(game, -1*inf, inf)
-        return self.alpha_beta_search(game, 3)
+        # Robot generates an action via alpha beta search
+        cls()
+        print "Robot is making a move..."
+        action = self.alpha_beta_search(game, game.difficulty*2 + 1)
+        if action[0].external:
+            p = game.current_player.stacks[action[0].x].pop()
+            p.external = False
+            game[action[1].y][action[1].x].push(p)
+        else:
+            p = game[action[0].y][action[0].x].pop()
+            game[action[1].y][action[1].x].push(p)
+
+        p, q = action[0], action[1]
+        game.history.append((p,q))
+        print "Robot moves from ({},{}) to ({}, {})".format(p.x, p.y, q.x, q.y)
+        print game
+        raw_input("[Press enter to start next turn]")
 
     # Evaluation function for this should be the number of possibile 4-in-a-rows
     # with current player's pieces
@@ -186,11 +208,11 @@ class Robot(Player):
     def alpha_beta_search(self, state, d): # accepts a depth parameter
         acts =  state.actions()
         if self == state.p1: # MAX
-            possibilities = [self.min_val(state.next(a), -1*inf, inf, d) for a in acts]
+            possibilities = [self.max_val(state.next(a), -1*inf, inf, state.turns + d) for a in acts]
             return acts[possibilities.index(max(possibilities))]
 
         else: # MIN
-            possibilities = [self.max_val(state.next(a), -1*inf, inf, d) for a in acts]
+            possibilities = [self.min_val(state.next(a), -1*inf, inf, state.turns + d) for a in acts]
             return acts[possibilities.index(min(possibilities))]
 
     # used in Alpha-beta
@@ -198,29 +220,29 @@ class Robot(Player):
         end_game = state.terminal_test()
         if end_game:
             return end_game
-        elif d == 0:
+        elif d == state.turns:
             return state.eval()
         else:
             v = -1 * inf
             for action in state.actions():
-                mval = self.min_val(state.next(action), a, b)
+                mval = self.min_val(state.next(action), a, b, d)
                 if mval > v:
                     v = mval
                 if v >= b:
                     return v
                 a = v if v > a else a
-
             return v
 
-    # used in Alpha-beta
     def min_val(self, state, a, b, d):
         end_game = state.terminal_test()
         if end_game:
             return end_game
+        elif d == state.turns:
+            return state.eval()
         else:
             v = inf
             for action in state.actions():
-                mval = self.min_val(state.next(action), a, b)
+                mval = self.max_val(state.next(action), a, b, d)
                 if mval < v:
                     v = mval
                 if v <= a:
@@ -228,7 +250,7 @@ class Robot(Player):
                 b = v if v < b else b
             return v
 
-    # Minimax implementation. Never terminates because depth is so large!
+    # used in Alpha-beta
     def minimax(self, state):
 
         def max_val(state):
@@ -250,7 +272,7 @@ class Robot(Player):
             else:
                 v = inf
                 for a in state.actions():
-                    mval = min_val(state.next(a))
+                    mval = max_val(state.next(a))
                     if mval < v:
                         v = mval
                 return v
@@ -277,12 +299,15 @@ class Piece:
         return str(self.owner) + str(self.size)
 
 class Game:
-    def __init__(self, p1, p2, current_player):
+    def __init__(self, p1, p2, level, time):
         self.board = []
-        self.turns = 0 # keeping track of many turns have passed in the game
+        self.history = []
+        self.difficulty = level
+        self.time_limit = time
+        self.turns = 1 # keeping track of many turns have passed in the game
         self.p1 = p1
         self.p2 = p2
-        self.current_player = self.p1 if self.p1.id == current_player else self.p2
+        self.current_player = self.p1
         for i in range(0, 4):
             self.board.append(list()) # new row
             for j in range(0, 4):
@@ -317,47 +342,56 @@ class Game:
             print self.p2.id + " won the game!"
 
         print self
+        raw_input("[Press enter to print move history]")
+        for p,q in self.history:
+            print "{} ({},{}) to ({}, {})".format(str(p)[0] + ": ", p.x, p.y, q.x, q.y)
 
     # function that gets all of the pieces of a player that are a part of a 3-in-a-row
-    def threes(self, pid):
+    def ninrow(self, pid, n):
         # Horizontal lines
         valid_horiz = []
         for row in self.board:
             possible_res = list(filter(lambda x: x.peek() and x.peek().owner == pid, row))
-            if len(possible_res) == 3:
+            if len(possible_res) == n:
                 valid_horiz += possible_res
 
         # Vertical lines
         valid_vert = []
         for i in range(0, 4):
-            col = [self.board[j][i] for j in range(0,4)]
+            col = [self.board[j][i] for j in range(0, 4)]
             possible_res = list(filter(lambda x: x.peek() and x.peek().owner == pid, col))
-            if len(possible_res) == 3:
+            if len(possible_res) == n:
                 valid_vert += possible_res
 
         # Valid diag (upperleft to bottom right)
         valid_diag = []
-        diag = [self.board[j][j] for j in range(0,4)]
+        diag = [self.board[j][j] for j in range(0, 4)]
         possible_res = list(filter(lambda x: x.peek() and x.peek().owner == pid, diag))
-        if len(possible_res) == 3:
+        if len(possible_res) == n:
             valid_diag += possible_res
 
         # Valid diag (bottom left to upperright)
-        diag = [self.board[j][j] for j in range(3,-1,-1)]
+        diag = [self.board[j][j] for j in range(3, -1, -1)]
         possible_res = list(filter(lambda x: x.peek() and x.peek().owner == pid, diag))
-        if len(possible_res) == 3:
+        if len(possible_res) == n:
             valid_diag += possible_res
 
         combined_stacks = valid_diag + valid_vert + valid_horiz
-        pieces = [s.peek() for s in combined_stacks]
-        return list(set(pieces)) # cleanse duplicates
+        # pieces = [s.peek() for s in combined_stacks]
+        return list(set(combined_stacks))  # cleanse duplicates
+
+    def threes(self, pid):
+        return self.ninrow(pid,3)
+
+    def twos(self, pid):
+        return self.ninrow(pid, 2)
 
     # Alpha-beta functions. Functions are heavily based off of templates
     # given in Artificial Intelligence: A Modern Approach (3rd Edition) p. 170
 
     # evaluation function to award a value to some cut off node
     def eval(self):
-        return 0
+        return len(self.threes(self.current_player))*3 + len(self.twos(self.current_player))
 
     # Returns 0 if p1 wins, 1 if p2 wins, and -1 if no winner yet
     def terminal_test(self):
@@ -384,9 +418,9 @@ class Game:
         p2_win = winner(self.p2.id)
 
         if p1_win:
-            return 1 # p1 wins
+            return self.turns * 2 # p1 wins
         elif p2_win:
-            return -1 # p2 wins
+            return self.turns * -2 # p2 wins
         else:
             return 0 # not a terminal state
 
@@ -406,7 +440,7 @@ class Game:
         if piece.external_piece:
             # if it's an external piece, we can only cover enemy pieces that are part of a three-in-a-row
             # and whose size is smaller than our piece in question
-            valid_locations += [x for x in self.threes(self.current_player.id) if x.size < piece.size]
+            valid_locations += [x for x in self.threes(self.current_player.id) if x.peek().size < piece.size]
         else:
             # if it's not an external piece, then we can cover any enemy piece that's smaller than ours
             valid_locations += [x for row in self.board for x in row if x.peek() != [] and (x.peek().owner != piece.owner and piece.size > x.peek().size)]
@@ -425,31 +459,46 @@ class Game:
     # of the piece to be moved and where it should be moved to.
     def next(self, action):
         next_game = deepcopy(self)
+        # print self.turns
         # Need to parse the x,y coords of both components of action. Again,
         # it is possible that our first component of action (the piece to move)
         # is from the external stack, so we need to check that...
+        # try:
         if action[0].external:
             p = next_game.current_player.stacks[action[0].x].pop()
-            next_game[action[1].x][action[1].y].push(p)
+            p.external = False
+            next_game[action[1].y][action[1].x].push(p)
         else:
-            next_game[action[1].x][action[1].y].push(next_game[action[1].x][action[1].y].pop())
+            p = next_game[action[0].y][action[0].x].pop()
+            next_game[action[1].y][action[1].x].push(p)
 
         next_game.turns += 1
         next_game.current_player = next_game.p2 if next_game.p1 == next_game.current_player else next_game.p1
-
         return next_game
+
+        # except Exception:
+        #     p1 = action[0]
+        #     q = action[1]
+        #     print "Failed to create new game state: "
+        #     print "Turn: ", str(self.turns)
+        #     print str(action[0].pieces)
+        #     print str(next_game[action[0].y][action[0].x].pieces)
+        #     print self
+        #     print next_game
+        #     # print "({},{}) to ({}, {})".format(p1.x, p1.y, q.x, q.y)
+        #     raw_input("")
 
 ## Players can have different character repesentations.
 # Their pieces on the board are represented as a string of the player ID
 # and the size of the piece. So, the smallest possible piece for player 'P' would be
 # represented by a string "P1". Ownership of pieces is also linked by this player ID.
 # So, player ID's cannot be the same! I am using P and Q for my example players.
-test_game = Game(Robot("Q"), Player("P"), "Q")
+# test_game = Game(Player("P"), Player("Q"), 0, 0)
 ## Terminal State Tests...
 ## Row test
-# test_game[0][0].push(Piece("P", 2))
-# test_game[0][1].push(Piece("P", 3))
-# test_game[0][2].push(Piece("P", 4))
+# test_game[0][0].push(Piece("Q", 2))
+# test_game[0][1].push(Piece("Q", 3))
+# test_game[0][2].push(Piece("Q", 4))
 # test_game[0][3].push(Piece("P", 4))
 ## Column test
 # test_game[0][0].push(Piece("P", 4))
@@ -459,9 +508,9 @@ test_game = Game(Robot("Q"), Player("P"), "Q")
 # test_game[2][0].push(Piece("P", 3))
 # test_game[3][0].push(Piece("P", 4))
 ## Diag test
-# test_game[0][0].push(Piece("P", 4))
-# test_game[1][1].push(Piece("P", 4))
-# test_game[2][2].push(Piece("P", 4))
+# test_game[0][0].push(Piece("Q", 4))
+# test_game[1][1].push(Piece("Q", 4))
+# test_game[2][2].push(Piece("Q", 4))
 # test_game[3][3].push(Piece("P", 4))
 ## Diag test 2
 # test_game[0][3].push(Piece("P", 4))
@@ -484,7 +533,23 @@ test_game = Game(Robot("Q"), Player("P"), "Q")
 # test_game.start()
 # for p, q in test_game.actions():
 #     print "({},{}) to ({}, {})".format(p.x, p.y, q.x,q.y)
-res = test_game.p1.play(test_game)
-print res[0].x, res[0].y
-print res[1].x, res[0].y
+#     print test_game.next((p,q))
 # test_game.start()
+
+def gobby(players, level, time):
+    g = None
+    if players == "hh":
+        g = Game(Player("P"), Player("Q"), level, time)
+    elif players == "hr":
+        g = Game(Player("P"), Robot("Q"), level, time)
+    elif players == "rh":
+        g = Game(Robot("P"), Player("Q"), level, time)
+    elif players == "rr":
+        g = Game(Robot("P"), Robot("Q"), level, time)
+    else:
+        print "Invalid player parameters!"
+        return 0
+
+    g.start()
+
+gobby("rr", 1, 100)
