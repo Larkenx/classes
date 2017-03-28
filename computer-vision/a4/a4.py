ce69307d326dc20ffc906b3b4f816a6fb8415024
larkenx <larkenx@gmail.com>
2017-03-28 00:49:00 -0400

    Automated commit

--- computer-vision/a4/a4.py
+++ computer-vision/a4/a4.py
@@ -14,7 +14,19 @@ def prepare_save(f):
     f_min = double(amin(f))
     # print f_max
     # print f_min
-    return uint8(( (f - f_min) / (f_max - f_min) * 255))
+    for i in range(0, f.shape[0]):
+        for j in range(0, f.shape[1]):
+            if f[i,j] == f_max:
+                f[i,j] = 255
+            elif f[i,j] > 1:
+                f[i,j] = 122.5
+            elif f[i,j] == 1:
+                f[i,j] = 50
+
+
+    return uint8(f)
+
+    # return uint8(( (f - f_min) / (f_max - f_min) * 255))
 
 # Problem 1
 
@@ -33,13 +45,14 @@ for i in range(0, canny_img.shape[0]):
     for j in range(0, canny_img.shape[1]):
         edge_point = canny_img[i,j]
         if edge_point:
-            for theta in range(0, 360 / bin_size ):
-                d = (j*cos(theta) + i*sin(theta)) / bin_size # divide by d bin size
+            for theta in range(0, 360 / bin_size):
+                d = uint8((j*sin(theta) + i*cos(theta)) / bin_size)# divide by d bin size
+                # print d
                 H[d,theta] += 1
 
-# imshow(H)
-# show()
-# Image.fromarray(prepare_save(H)).save('line_hough.jpg')
+imshow(H)
+show()
+Image.fromarray(prepare_save(H)).save('line_hough.jpg')
 
 
 
