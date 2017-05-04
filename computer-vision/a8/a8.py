82cc60bdd2340c553f5d9bc1c578baa86d6579a5
Steven Myers <larkenx@gmail.com>
2017-05-04 00:04:44 -0400

    working on a8

--- computer-vision/a8/a8.py
+++ computer-vision/a8/a8.py
@@ -3,8 +3,9 @@ from os import listdir
 from os.path import isfile, join
 import os
 import cv2
+import matplotlib.pyplot as plt
 
-project_dir = "/Users/larken/class/computer-vision/a8/"
+project_dir = os.getcwd() + '/'
 
 def draw_flow(im,flow,step=16):
     """ Plot optical flow at sample points
@@ -22,8 +23,8 @@ def draw_flow(im,flow,step=16):
         cv2.circle(vis,(x1,y1),1,(0,255,0), -1)
     return vis
 
-# Generating optical flow Dog frames
 '''
+# Generating optical flow Dog frames
 dog_path = "/Users/larken/class/computer-vision/a8/dog/"
 dog_frames = [f for f in listdir(dog_path)]
 flow_dog_path = "/Users/larken/class/computer-vision/a8/flow_dog/"
@@ -84,7 +85,7 @@ def get_HOG_of_frame(frame):
                 x_window = angle.shape[1]
             if j+h_offset >= angle.shape[0]:
                 y_window = angle.shape[0]
-            region = angle[i:x_window, j:y_window]
+            region = angle[j:y_window, i:x_window]
             # For this region, calculate gradient directions and store in HOG
             HOG = zeros_like(arange(9), dtype=uint8)
             for dir in region.flatten():
@@ -104,26 +105,68 @@ def get_HOG_of_frame(frame):
 def video_HOG_average(sequence_dir):
     label, frames = xyt(sequence_dir)
     average_HOG = zeros_like(arange(255))
-    i = 0
-    for f in frames:
-        print "Processing frame {0}".format(i)
-        averaged_HOG =+ get_HOG_of_frame(f)
-        i += 1
+    for f in range(0, frames.shape[0], 6):
+        # print "Processing frame {0}".format(f)
+        res = get_HOG_of_frame(frames[f])
+        averaged_HOG =+ res
 
-    return label, averaged_HOG / frames.shape[0]
+    return label, averaged_HOG / (frames.shape[0] / 6)
 
 jpl_path = project_dir + "jpl/"
-video_HOG_average(jpl_path + "1_1")
-
-# sequence_directories = [jpl_path + dir for dir in listdir(jpl_path) if dir != '.DS_Store']
-# activity_ids = []
+sequence_directories = [jpl_path + dir for dir in listdir(jpl_path) if dir != '.DS_Store']
+'''Averaging all of the HOG's from every video. Sampling every 6 frames.
+I wrote this to the disk because it took forever!'''
 # averaged_videos = []
 # for i in range(0, len(sequence_directories)):
 #     print "Processing directory {0}".format(sequence_directories[i])
 #     label, avg_HOG = video_HOG_average(sequence_directories[i])
-#     activity_ids.append(label)
 #     averaged_videos.append(avg_HOG)
-#
-# averaged_videos = array(averaged_videos)
 # savetxt("averaged_video_hogs", averaged_videos)
+
+'''Getting activity ID's from the videos'''
+# activity_ids = []
+# for i in range(0, len(sequence_directories)):
+#     sequence_dir = sequence_directories[i]
+#     label = int(sequence_dir.split('_')[1])
+#     activity_ids.append(label)
+
+# averaged_videos = array(averaged_videos)
+# averaged_videos = loadtxt("averaged_video_hogs")
+# labeled_avg_vids = zip(activity_ids, averaged_videos)
+#
+# labels, first_hog_avg = video_HOG_average(jpl_path + "1_1")
+# savetxt("first_hog_avg", first_hog_avg)
+# first_hog_avg = loadtxt("first_hog_avg")
+labels, sec_hog_avg = video_HOG_average(jpl_path + "2_2")
+savetxt("sec_hog_avg", sec_hog_avg)
+
+def show_hog(hog, fname):
+    x_offset = 50
+    y_offset = 50
+    i = 0
+    j = 0
+    for h in range(0,225,9):
+        spatial_region = hog[h:h+9]
+        center = (i + x_offset, j + y_offset)
+        for theta in range(0,9):
+            bin_count = spatial_region[theta]*5
+            angle = theta*20 + 20
+            endpoint = cos(angle)*bin_count + i + x_offset, sin(angle)*bin_count + j + y_offset
+            print endpoint
+            plt.plot([center[0], endpoint[0]], [center[1], endpoint[1]])
+        i += 100
+        if i >= 600:
+            i = 0
+            j += 100
+    plt.savefig(fname)
+    plt.show()
+
+# Visualizing averaged HOG results
+# show_hog(first_hog_avg, "1_1_hog")
+# show_hog(sec_hog_avg, "2_2_hog")
+
+
+
+
+
 
